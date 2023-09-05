5e54d88b8e5067fea1270bd2e5513abc218fe0ea
sapn1s <48549775+sapn1s@users.noreply.github.com>
2023-09-05 07:19:02 +0300

    Optimized version
    - Using ffmpeg instead of streamlink
    - Results in much smaller file sizes (approx 5x smaller)
    - Adjustable quality
    - More resiliant

--- README.md
+++ README.md
@@ -1,49 +1,60 @@
 # Twitch Stream Recorder
 
-A Python script to automatically detect when a Twitch user goes online and then records their stream using Streamlink.
+A Python script to automatically detect when a Twitch user goes online and then records their stream.
 
 ## Requirements
 
 - Python 3+
 - Streamlink
+- ffmpeg
 
 ## Configuration
 
 Before you begin, make sure to set up the `settings.ini` file with the required Twitch credentials. Here's a sample:
 
-```ini
-[TWITCH]
-CLIENT_ID = your_client_id
-CLIENT_SECRET = your_client_secret
-[STREAMERS]
-username = twitch_username
-```
+  ```ini
+  [TWITCH]
+  CLIENT_ID = your_client_id
+  CLIENT_SECRET = your_client_secret
+  [STREAMERS]
+  username = twitch_username
+  [OUTPUT]
+  quality=28
+  ```
 
 ## Installation
 
 1. Install the required Python packages:
-```
-pip install -r requirements.txt
-```
-
-2. Install and set up Streamlink:
-- **For Windows**: If you've installed `streamlink` using `pip`, you might need to add its installation path to the system's PATH variable. Commonly, this would be in a directory like `C:\Users\<Username>\AppData\Roaming\Python\Python<version>\Scripts`. Add this to your PATH.
-  
-- **For Linux**: If you've installed `streamlink` with `pip`, it should typically be available globally. If not, you might need to add it to your PATH. This can often be done with:
   ```
-  echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
-  source ~/.bashrc
+  pip install -r requirements.txt
+  ```
+
+2. Install and set up FFmpeg:
+- **For Windows**: Download FFmpeg from its official website and add the bin directory to your system's PATH variable.
+- **For Linux**: You can install FFmpeg using the package manager for your distribution, for example:
+  ```
+  sudo apt-get install ffmpeg
   ```
 
 3. Run the script:
-```
-python main.py
-```
+  ```
+  python main.py
+  ```
 
 ## How It Works
 
 1. The script checks if the specified Twitch user (from `settings.ini`) is online every 2 minutes.
 2. Once the user is detected online, it fetches the best stream URL.
-3. It then uses Streamlink to record the stream.
-4. The recorded stream is saved in the `outputs` directory with the format: `username_DD-MM-YY_HH-MM.mp4`.
+3. It then uses FFmpeg to record the stream. The stream is saved as a `.ts` file format because it's more resilient to interruptions than other formats.
+4. The recorded stream is saved in the `outputs` directory with the format: `username_DD-MM-YY_HH-MM.ts`.
 5. If any errors occur, it retries with an exponential backoff strategy.
+
+## Quality Settings
+
+The quality of the recorded stream can be adjusted in the `settings.ini` file under the `[OUTPUT]` section by setting the `quality` parameter. This corresponds to the CRF (Constant Rate Factor) value used by the x264 encoder. The value can range from 0-51:
+
+- CRF 0: Lossless compression (largest file size).
+- CRF 23: Default value in FFmpeg, a balance between file size and quality.
+- CRF 51: Worst quality but smallest file size.
+
+The default value in this script is set to 28, offering a good trade-off between file size and video quality.
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import configparser
 import requests
-import time
+import sys
 import os
 import streamlink
 from datetime import datetime
@@ -14,59 +14,109 @@ output_folder = './outputs'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-MAX_WAIT_TIME = 300 
+MAX_WAIT_TIME = 300
+
+
+async def wait_for_user_input(stop_event):
+    """Wait for user input and then set the stop_event."""
+    input("Press Enter to stop the recording...")
+    await stop_event.set()
+
+
+async def record_stream(stream_url, filename, stop_event):
+    """Record the stream and listen for the stop event."""
+    ffmpeg_cmd = [
+        'ffmpeg', '-i', stream_url,
+        '-loglevel', 'error',
+        '-c:v', 'libx264', '-preset', 'faster', '-crf', config['OUTPUT']['quality'],
+        '-c:a', 'aac', '-b:a', '128k',
+        '-bsf:a', 'aac_adtstoasc',
+        f'{filename}'
+    ]
+    #can be converted to MP4 using the following command (reduces the file size as well)
+    #ffmpeg -i input.ts -c:v copy -c:a copy output.mp4
+
+    process = await asyncio.create_subprocess_exec(*ffmpeg_cmd)
+    print("Streaming into", filename)
+
+    process_task = asyncio.create_task(process.communicate())
+    stop_event_task = asyncio.create_task(stop_event.wait())
+
+    # Wait for the process to complete or for stop_event to be set
+    done, pending = await asyncio.wait(
+        [process_task, stop_event_task],
+        return_when=asyncio.FIRST_COMPLETED
+    )
+
+    # If the stop_event was set, we'll terminate the process
+    if stop_event.is_set():
+        print("Terminating...")
+        process.terminate()
+        print("Waiting...")
+        await process.wait()
+        print("Done...")
+
 
 async def main():
     username = config["STREAMERS"]["username"]
     wait_time = 1
+
+    stop_event = asyncio.Event()
+    input_task = asyncio.create_task(wait_for_user_input(stop_event))
+
     while True:
         try:
             streaming = False
             while not streaming:
                 print(f"Checking if {username} is online...")
-                token = get_twitch_token(config['TWITCH']['CLIENT_ID'], config['TWITCH']['CLIENT_SECRET'])
+                token = get_twitch_token(
+                    config['TWITCH']['CLIENT_ID'], config['TWITCH']['CLIENT_SECRET'])
                 streaming = check_streaming(token, username)
-                if(streaming):
+                if (streaming):
                     break
                 await asyncio.sleep(120)
-            #Online
+            # Online
             print(f"{username} is online!")
             stream_url = get_stream_url(username)
             print("Stream URL:", stream_url)
-            if(stream_url):
-                #record stream
-                filename = f"{output_folder}/{username}_{datetime.now().strftime('%d-%m-%y_%H-%M')}.mp4"
-                streamlink_cmd = ['streamlink', stream_url, 'best', '-o', filename]
-                process = await asyncio.create_subprocess_exec(*streamlink_cmd)
-                print("Streaming into", filename)
-                await process.communicate()
-                #Stream ended or any other issue
+            if (stream_url):
+                # record stream
+                filename = f"{output_folder}/{username}_{datetime.now().strftime('%d-%m-%y_%H-%M')}.ts"
+                await record_stream(stream_url, filename, stop_event)
+                if stop_event.is_set():
+                    break
+
         except Exception as e:
             print("Error occured:", e)
             traceback.print_exc()
             print(f"Retrying in {wait_time} seconds...")
             await asyncio.sleep(wait_time)
             wait_time = min(wait_time * 2, MAX_WAIT_TIME)
-
+        finally:
+            # Cancel the input task if it's still running
+            input_task.cancel()
 
 
 def check_streaming(token, username):
     headers = {
-    'Client-ID': config['TWITCH']['CLIENT_ID'],
-    'Authorization': f'Bearer {token}'
+        'Client-ID': config['TWITCH']['CLIENT_ID'],
+        'Authorization': f'Bearer {token}'
     }
-    response = requests.get(f'https://api.twitch.tv/helix/users?login={username}', headers=headers)
+    response = requests.get(
+        f'https://api.twitch.tv/helix/users?login={username}', headers=headers)
     response_data = response.json()
     user_id = response_data['data'][0]['id']
 
     # Then, check if the user is currently streaming
-    response = requests.get(f'https://api.twitch.tv/helix/streams?user_id={user_id}', headers=headers)
+    response = requests.get(
+        f'https://api.twitch.tv/helix/streams?user_id={user_id}', headers=headers)
     response_data = response.json()
 
     if response_data['data']:
         return True
     else:
         return False
+
 
 def get_stream_url(username):
     url = f"https://www.twitch.tv/{username}"
